9fcc69e3837ea6c0882ee7e77ebf207825fd6410
k <k@localhost>
2026-08-02 15:10:40 +0800

    feat: project skeleton with runnable empty window

---
+++ src/ui/MainWindow.cpp
@@ -0,0 +1,10 @@
+#include "MainWindow.h"
+#include <QLabel>
+
+MainWindow::MainWindow(QWidget* parent)
+    : QMainWindow(parent)
+{
+    setWindowTitle(QStringLiteral("关节模组监控台"));
+    setCentralWidget(new QLabel(QStringLiteral("骨架窗口"), this));
+    resize(960, 600);
+}
